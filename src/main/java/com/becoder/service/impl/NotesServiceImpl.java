88a1b06fc5182619132bd9f45164647fb8485316
Pavy <daspabitra55@gmail.com>
2024-10-28 21:17:02 +0530

    save notes

---
+++ src/main/java/com/becoder/service/impl/NotesServiceImpl.java
@@ -0,0 +1,55 @@
+package com.becoder.service.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import com.becoder.dto.NotesDto;
+import com.becoder.dto.NotesDto.CategoryDto;
+import com.becoder.entity.Category;
+import com.becoder.entity.Notes;
+import com.becoder.exception.ResourceNotFoundException;
+import com.becoder.repository.CategoryRepository;
+import com.becoder.repository.NotesRepository;
+import com.becoder.service.NotesService;
+
+@Service
+public class NotesServiceImpl implements NotesService {
+
+	@Autowired
+	private NotesRepository notesRepo;
+
+	@Autowired
+	private ModelMapper mapper;
+
+	@Autowired
+	private CategoryRepository categoryRepo;
+
+	@Override
+	public Boolean saveNotes(NotesDto notesDto) throws Exception {
+
+		// category validation
+		checkCategoryExist(notesDto.getCategory());
+
+		Notes notes = mapper.map(notesDto, Notes.class);
+		Notes saveNotes = notesRepo.save(notes);
+		if (!ObjectUtils.isEmpty(saveNotes)) {
+			return true;
+		}
+		return false;
+	}
+
+	private void checkCategoryExist(CategoryDto category) throws Exception {
+		categoryRepo.findById(category.getId()).orElseThrow(() -> new ResourceNotFoundException("category id invalid"));
+	}
+
+	@Override
+	public List<NotesDto> getAllNotes() {
+		return notesRepo.findAll().stream().map(note -> mapper.map(note, NotesDto.class)).toList();
+	}
+
+}
