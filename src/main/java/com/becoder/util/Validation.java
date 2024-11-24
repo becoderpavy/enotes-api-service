cba454c37ad4022916f2f6046e8f23e002701d35
Pavy <daspabitra55@gmail.com>
2024-11-25 00:28:09 +0530

    email exist

--- src/main/java/com/becoder/util/Validation.java
+++ src/main/java/com/becoder/util/Validation.java
@@ -16,15 +16,20 @@ import com.becoder.dto.TodoDto.StatusDto;
 import com.becoder.dto.UserDto;
 import com.becoder.entity.Role;
 import com.becoder.enums.TodoStatus;
+import com.becoder.exception.ExistDataException;
 import com.becoder.exception.ResourceNotFoundException;
 import com.becoder.exception.ValidationException;
 import com.becoder.repository.RoleRepository;
+import com.becoder.repository.UserRepository;
 
 @Component
 public class Validation {
 
 	@Autowired
 	private RoleRepository roleRepo;
+
+	@Autowired
+	private UserRepository userRepo;
 
 	public void categoryValidation(CategoryDto categoryDto) {
 
@@ -61,7 +66,6 @@ public class Validation {
 				}
 			}
 		}
-
 		if (!error.isEmpty()) {
 			throw new ValidationException(error);
 		}
@@ -93,6 +97,12 @@ public class Validation {
 
 		if (!StringUtils.hasText(userDto.getEmail()) || !userDto.getEmail().matches(Constants.EMAIL_REGEX)) {
 			throw new IllegalArgumentException("email is invalid");
+		} else {
+			// validate email exist
+			Boolean existEmail = userRepo.existsByEmail(userDto.getEmail());
+			if (existEmail) {
+				throw new ExistDataException("Email already exist");
+			}
 		}
 
 		if (!StringUtils.hasText(userDto.getMobNo()) || !userDto.getMobNo().matches(Constants.MOBNO_REGEX)) {
@@ -111,7 +121,6 @@ public class Validation {
 			if (!CollectionUtils.isEmpty(invalidReqRoleids)) {
 				throw new IllegalArgumentException("role is invalid" + invalidReqRoleids);
 			}
-
 		}
 
 	}
