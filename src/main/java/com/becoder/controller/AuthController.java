61c44482a52169a6ba123f30126c2d2cf3edd687
Pavy <daspabitra55@gmail.com>
2024-12-01 03:21:38 +0530

    login implement

--- src/main/java/com/becoder/controller/AuthController.java
+++ src/main/java/com/becoder/controller/AuthController.java
@@ -3,11 +3,14 @@ package com.becoder.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.becoder.dto.LoginRequest;
+import com.becoder.dto.LoginResponse;
 import com.becoder.dto.UserDto;
 import com.becoder.service.UserService;
 import com.becoder.util.CommonUtil;
@@ -23,12 +26,22 @@ public class AuthController {
 
 	@PostMapping("/")
 	public ResponseEntity<?> registerUser(@RequestBody UserDto userDto, HttpServletRequest request) throws Exception {
-		String url=CommonUtil.getUrl(request);
-		Boolean register = userService.register(userDto,url);
+		String url = CommonUtil.getUrl(request);
+		Boolean register = userService.register(userDto, url);
 		if (register) {
 			return CommonUtil.createBuildResponseMessage("Register success", HttpStatus.CREATED);
 		}
 		return CommonUtil.createErrorResponseMessage("Register failed", HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@PostMapping("/login")
+	public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) throws Exception {
+
+		LoginResponse loginResponse = userService.login(loginRequest);
+		if (ObjectUtils.isEmpty(loginResponse)) {
+			return CommonUtil.createErrorResponseMessage("invalid credential", HttpStatus.BAD_REQUEST);
+		}
+		return CommonUtil.createBuildResponse(loginResponse,HttpStatus.OK);
 	}
 
 }
