c25e6323af9ae1b2827c20d666aee1702c04264a
Pavy <daspabitra55@gmail.com>
2025-05-02 00:24:02 +0530

    actuator implement

--- src/main/java/com/becoder/config/security/SecurityConfig.java
+++ src/main/java/com/becoder/config/security/SecurityConfig.java
@@ -48,13 +48,11 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.csrf(csrf -> csrf.disable())
-				.authorizeHttpRequests(
-						req -> req
-								.requestMatchers("/api/v1/home/**", "/api/v1/auth/**", "/swagger-ui/**",
-										"/v3/api-docs/**", "/enotes-doc/**", "/enotes-api-doc/**")
-								.permitAll().anyRequest().authenticated())
-				.httpBasic(Customizer.withDefaults())
+		http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(req -> req.requestMatchers("/api/v1/home/**",
+				"/api/v1/auth/**", "/swagger-ui/**", "/v3/api-docs/**", "/enotes-doc/**", 
+				"/enotes-api-doc/**","/actuator/**"
+
+		).permitAll().anyRequest().authenticated()).httpBasic(Customizer.withDefaults())
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
